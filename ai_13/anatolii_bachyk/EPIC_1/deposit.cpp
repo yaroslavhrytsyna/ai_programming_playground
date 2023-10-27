87aa4748b8681c1deadbb6147ade6ef349298c6c
Bachyk Anatolii <admin@MacBook-Pro-admin.local>
2023-10-27 13:55:18 +0300

    changes

--- ai_13/anatolii_bachyk/EPIC_1/deposit.cpp
+++ ai_13/anatolii_bachyk/EPIC_1/deposit.cpp
@@ -7,12 +7,12 @@ using namespace std;
 int main () {
 
 string name;
-int amount, number, times, interest;
+int principal_amount, number, times, interest;
 float sum_1, sum_2;
 cout << "Enter your name:" << endl;
     cin >> name;
 cout << "Enter the principal amount:" << endl;
-    cin >> amount;
+    cin >> principal_amount;
 cout << "Enter the rate of interest:" << endl;
     cin >> interest;
 cout << "Enter the number of years:" << endl;
@@ -20,11 +20,11 @@ cout << "Enter the number of years:" << endl;
 cout << "Enter the number of times interest is compounded per year:" << endl;
     cin >> times;
 
-    sum_1=amount*pow((1+(0.01*interest/times)), number*times) ;
-    sum_2=sum_1-amount;
+    sum_1=principal_amount * pow((1+(0.01*interest/times)), number*times) ;
+    sum_2=sum_1- principal_amount;
 
 
     cout << "Hello, " << name << "!" << endl;
-    cout << "The compound interest for a principal of " << amount << " at a rate of " << interest << "% compounded " << times << " times a year for " << number << " years is: "<< sum_2 << ". The total amount after " << number << " years is: " << sum_1 << endl;   
+    cout << "The compound interest for a principal of " << principal_amount << " at a rate of " << interest << "% compounded " << times << " times a year for " << number << " years is: "<< sum_2 << ". The total amount after " << number << " years is: " << sum_1 << endl;   
     return 0;
 }
