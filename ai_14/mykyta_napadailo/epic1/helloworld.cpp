49e1603ee030550a491dfb3d5129e7083bcc67be
mykytanpdl <mykyta.napadailo.shi.2023@lpnu.ua>
2023-10-29 17:54:13 +0200

    Added helloworld.cpp and task.cpp(compound interest)

---
+++ ai_14/mykyta_napadailo/epic1/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  // Include the input/output stream library
+
+int main() {
+    // Declare a string variable named 'text' and initialize it with the text "Hello World!"
+    std::string text = "Hello World!";
+    // Output the contents of the 'text' variable to the console using 'std::cout'
+    std::cout << text; 
+    // Return 0 to indicate a successful program execution
+    return 0;
+}
